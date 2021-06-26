65ba31122ba5b988ba33fd6db9a0969c8af18f9d
Sebastián Salazar <sebasalazar@gmail.com>
2021-06-26 15:34:40 -0400

    Agregamos la operación de venta

--- app/src/main/java/cl/utem/infb8090/api/persistence/repository/SaleRepository.java
+++ app/src/main/java/cl/utem/infb8090/api/persistence/repository/SaleRepository.java
@@ -1,10 +1,14 @@
 package cl.utem.infb8090.api.persistence.repository;
 
+import cl.utem.infb8090.api.persistence.model.Product;
 import cl.utem.infb8090.api.persistence.model.Sale;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+    public List<Sale> findByProduct(Product product);
 
 }
