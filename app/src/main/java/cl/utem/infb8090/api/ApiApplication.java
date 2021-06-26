2ecaa3d116971a578fe7daec7de7d3b5ab5a20db
Sebastián Salazar <sebasalazar@gmail.com>
2021-06-26 11:23:34 -0400

    Proyecto spring boot inicial

---
+++ app/src/main/java/cl/utem/infb8090/api/ApiApplication.java
@@ -0,0 +1,13 @@
+package cl.utem.infb8090.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
