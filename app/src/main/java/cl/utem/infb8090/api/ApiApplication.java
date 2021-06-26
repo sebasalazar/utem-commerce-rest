e583f2729938f75854bc448039505404c82c11d3
Sebastián Salazar <sebasalazar@gmail.com>
2021-06-26 15:17:32 -0400

    Agregamos filtro CORS - Importante para la aplicación móvil

--- app/src/main/java/cl/utem/infb8090/api/ApiApplication.java
+++ app/src/main/java/cl/utem/infb8090/api/ApiApplication.java
@@ -1,10 +1,21 @@
 package cl.utem.infb8090.api;
 
+import cl.utem.infb8090.api.rest.filter.SimpleCorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiApplication {
+
+    @Bean
+    public FilterRegistrationBean<SimpleCorsFilter> simpleCorsFilter() {
+        FilterRegistrationBean<SimpleCorsFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new SimpleCorsFilter());
+        registrationBean.addUrlPatterns("/v1/");
+        return registrationBean;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
